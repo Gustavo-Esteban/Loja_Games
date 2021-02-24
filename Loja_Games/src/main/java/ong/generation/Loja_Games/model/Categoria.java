cd331b6da7cc35b98025de8b18acc9873b288f80
Esteban <gustavoesteban13@hotmail.com>
2021-02-24 10:06:40 -0300

    Criação do CRUD de categoria

--- Loja_Games/src/main/java/ong/generation/Loja_Games/model/Categoria.java
+++ Loja_Games/src/main/java/ong/generation/Loja_Games/model/Categoria.java
@@ -1,5 +1,56 @@
 package ong.generation.Loja_Games.model;
 
-public class Categoria {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table( name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 }
--- Loja_Games/src/main/java/ong/generation/Loja_Games/repository/CategoriaRepository.java
+++ Loja_Games/src/main/java/ong/generation/Loja_Games/repository/CategoriaRepository.java
@@ -1,5 +1,12 @@
 package ong.generation.Loja_Games.repository;
 
-public class CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ong.generation.Loja_Games.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	public List<Categoria> findAllByNomeContainingIgnoreCase( String nome);
 }
