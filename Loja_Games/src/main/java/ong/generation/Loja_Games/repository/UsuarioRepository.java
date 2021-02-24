c8ec67dd5b5f1e64faaf523cf2aff8d29292584c
Esteban <gustavoesteban13@hotmail.com>
2021-02-24 11:15:01 -0300

    Inserindo Repository Usuario

--- Loja_Games/src/main/java/ong/generation/Loja_Games/repository/UsuarioRepository.java
+++ Loja_Games/src/main/java/ong/generation/Loja_Games/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package ong.generation.Loja_Games.repository;
 
-public class UsuarioRepository {
+import java.util.List;
 
+import ong.generation.Loja_Games.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
+	
+	public List<Usuario> findAllByNomeCompletoContainingIgnoreCase( String nomeCompleto);
 }
