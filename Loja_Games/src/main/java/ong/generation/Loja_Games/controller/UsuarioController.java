5fd7886df7463f2ea1be80100ab8862db0384d55
Esteban <gustavoesteban13@hotmail.com>
2021-02-24 11:18:16 -0300

    Inserindo Controller Usuario

--- Loja_Games/src/main/java/ong/generation/Loja_Games/controller/UsuarioController.java
+++ Loja_Games/src/main/java/ong/generation/Loja_Games/controller/UsuarioController.java
@@ -1,5 +1,61 @@
 package ong.generation.Loja_Games.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ong.generation.Loja_Games.model.Usuario;
+import ong.generation.Loja_Games.repository.UsuarioRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/usuario")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByName(@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeCompletoContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario){
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id){
+		repository.deleteById(id);		
+	}
 
 }
+
