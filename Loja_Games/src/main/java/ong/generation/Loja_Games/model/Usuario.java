0c8001f90e63a3be1079844c6e93e17adcd30706
Esteban <gustavoesteban13@hotmail.com>
2021-02-24 11:13:03 -0300

    Inserindo Model Usuario

--- Loja_Games/src/main/java/ong/generation/Loja_Games/model/Usuario.java
+++ Loja_Games/src/main/java/ong/generation/Loja_Games/model/Usuario.java
@@ -1,5 +1,79 @@
 package ong.generation.Loja_Games.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nomeCompleto;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Usuario> usuario;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome_Completo() {
+		return nomeCompleto;
+	}
+
+	public void setNome_Completo(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Usuario> getPostagem() {
+		return usuario;
+	}
+
+	public void setPostagem(List<Usuario> usuario) {
+		this.usuario = usuario;
+	}	
 
 }
+
