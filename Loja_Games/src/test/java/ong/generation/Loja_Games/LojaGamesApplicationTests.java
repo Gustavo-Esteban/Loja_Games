30cc2cba2b3445d62302d3551f682b2770a47f0d
Esteban <gustavoesteban13@hotmail.com>
2021-02-24 09:00:21 -0300

    Criação do projeto

---
+++ Loja_Games/src/test/java/ong/generation/Loja_Games/LojaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package ong.generation.Loja_Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
